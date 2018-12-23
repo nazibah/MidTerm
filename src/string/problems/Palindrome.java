42f06b571601a53e47ac77f61a10b1de447aba87
nazibah <nazibah95@gmail.com>
2018-12-23 18:53:39 -0500

    Fourth commit

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -6,6 +6,30 @@ public class Palindrome {
           If a String is reversed and it remains unchanged, that is called Palindrome. For example, MOM,DAD,MADAM are
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
+        String inputString = "Naz";
 
+
+        int length = inputString.length();
+        int i, begin, end, middle;
+
+        begin = 0;
+        end = length - 1;
+        middle = (begin + end)/2;
+
+        for (i = begin; i <= middle; i++) {
+            if (inputString.charAt(begin) == inputString.charAt(end)) {
+                begin++;
+                end--;
+            }
+            else {
+                break;
+            }
+        }
+        if (i == middle + 1) {
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not a palindrome");
+        }
     }
 }
