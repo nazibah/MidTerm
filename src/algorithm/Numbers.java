7fb9d4053af3a3983b18b195d041cc205591f08f
nazibah <nazibah95@gmail.com>
2018-12-23 22:14:37 -0500

    Seventh commit

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -1,6 +1,6 @@
 package algorithm;
 
-import databases.ConnectToSqlDB;
+import databases.ConnectDB;
 
 import java.util.List;
 import java.util.Random;
@@ -14,36 +14,94 @@ public class Numbers {
 	 * Show all the different kind of sorting algorithm by applying into (num array).
 	 * Display the execution time for each sorting.Example in below.
 	 *
-	 * Use any databases[MongoDB, Oracle or MySql] to store data and retrieve data.
+	 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 	 *
 	 * At the end. After running all the sorting algo, come to a conclusion which one is suitable on given data set.
 	 *
 	 */
 
 	public static void main(String[] args) throws Exception {
-		
+
 		int [] num = new int[10];
+
 		storeRandomNumbers(num);
-		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+		ConnectDB connectDB = new ConnectDB();
+
 		//Selection Sort
 		Sort algo = new Sort();
 		algo.selectionSort(num);
+
+		//selection Sort
 		long selectionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
-        connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
-        List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
-        printValue(numbers);
+		connectDB.insertDataFromArrayToMySql(num, "selection_sort", "SortingNumbers");
+		List<String> numbers = connectDB.readDataBase("selection_sort", "SortingNumbers");
+		printValue(numbers);
 		int n = num.length;
 		randomize (num, n);
+
 		//Insertion Sort
 		algo.insertionSort(num);
 		long insertionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "Insertion_sort", "SortingNumbers");
 
-		//By following above, Continue for rest of the Sorting Algorithm....
+		printValue(numbers);
+		randomize (num, n);
 
+		//bubble Sort
+		algo.bubbleSort(num);
+		long bubbleSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Bubble Sort take: " + bubbleSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "Bubble_sort", "SortingNumbers");
 
+		printValue(numbers);
+		randomize (num, n);
 
+		//Heap Sort
+		algo.heapSort(num);
+		long heapSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Heap Sort take: " + heapSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "Heap_sort", "SortingNumbers");
+
+		printValue(numbers);
+		randomize (num, n);
+
+		//Bucket sort
+		algo.bucketSort(num);
+		long bucketSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in bucket Sort take: " + bucketSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "bucket_sort", "SortingNumbers");
+
+		printValue(numbers);
+		randomize (num, n);
+
+		//quick sort
+		algo.quickSort(num, findLow(num), findHigh(num));
+		long quickSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Quick Sort take: " + quickSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "quick_sort", "SortingNumbers");
+
+		printValue(numbers);
+		randomize (num, n);
+
+		//merge Sort
+		algo.MergeSort(num);
+		long mergeSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in merge Sort take: " + mergeSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "merge_sort", "SortingNumbers");
+
+		printValue(numbers);
+		randomize (num, n);
+
+		//Shell Short
+		algo.shellSort(num);
+		long shellSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Shell Sort take: " + shellSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "Shell_sort", "SortingNumbers");
+
+		printValue(numbers);
+		randomize (num, n);
 
 		//Come to conclusion about which Sorting Algo is better in given data set.
 
@@ -52,8 +110,28 @@ public class Numbers {
 	public static void storeRandomNumbers(int [] num){
 		Random rand = new Random();
 		for(int i=0; i<num.length; i++){
-			num[i] = rand.nextInt(1000000);
+			num[i] = rand.nextInt(10);
 		}
+	}
+
+	public static int findLow(int[] array){
+		int low = array[0];
+		for(int n: array){
+			if(array[n] < low){
+				low = array[n];
+			}
+		}
+		return low;
+	}
+
+	public static int findHigh(int[] array){
+		int high = array[0];
+		for(int n: array){
+			if(array[n] > high){
+				high = array[n];
+			}
+		}
+		return high;
 	}
 
 	public static void randomize( int arr[], int n)
@@ -68,6 +146,7 @@ public class Numbers {
 			arr[j] = temp;
 		}
 	}
+
 	public static void printValue(List<String> array){
 		for(String st:array){
 			System.out.println(st);
